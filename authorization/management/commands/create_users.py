90ef817cd11949fcfabfa1d42657dd082e9bf5a6
Carlos Flores <cafg10@gmail.com>
2016-02-22 08:18:04 -0600

    Adding user creation command

--- authorization/management/commands/create_users.py
+++ authorization/management/commands/create_users.py
@@ -1,8 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from bridge.models import Affiliate
 from django.core.management.base import BaseCommand
+from django.db.models.aggregates import Max
+from django.utils.translation import ugettext_lazy as _
+
+from authorization.models import User
 
 
 class Command(BaseCommand):
-    help = "My shiny new management command."
+    help = "Creates the missing users based in the :class:`Affiliate` data"
 
     def handle(self, *args, **options):
-        raise NotImplementedError()
+        max_created = User.objects.aggregate(
+                max=Max('affiliate_id')
+        )['max']
+
+        affiliates = Affiliate.objects.filter(
+                id__gt=max_created,
+                card_id__isnull=False,
+        )
+
+        print(_("Creando {0} usuarios").format(affiliates.count()))
+        print(_('Iniciando con el {0}').format(max_created + 1))
+
+        [User.objects.create_user(
+                '{0}'.format(a.id),
+                a.email,
+                a.card_id,
+                first_name=a.first_name,
+                last_name=a.last_name,
+                affiliate=a,
+        ) for a in affiliates]
