b063e431d6699dd71c636c7bc6a9439dc2b44985
Carlos Flores <cafg10@gmail.com>
2016-03-03 11:48:17 -0600

    Allow mass user creation

--- authorization/management/commands/create_users.py
+++ authorization/management/commands/create_users.py
@@ -5,6 +5,7 @@ from bridge.models import Affiliate
 from django.core.management.base import BaseCommand
 from django.db.models.aggregates import Max
 from django.utils.translation import ugettext_lazy as _
+import re
 
 from authorization.models import User
 
@@ -13,9 +14,13 @@ class Command(BaseCommand):
     help = "Creates the missing users based in the :class:`Affiliate` data"
 
     def handle(self, *args, **options):
+        non_decimal = re.compile(r'[^\d.]+')
+
         max_created = User.objects.aggregate(
                 max=Max('affiliate_id')
         )['max']
+        if max_created is None:
+            max_created = 0
 
         affiliates = Affiliate.objects.filter(
                 id__gt=max_created,
@@ -28,7 +33,7 @@ class Command(BaseCommand):
         [User.objects.create_user(
                 '{0}'.format(a.id),
                 a.email,
-                a.card_id,
+                re.sub("[^0-9]", "", a.card_id),
                 first_name=a.first_name,
                 last_name=a.last_name,
                 affiliate=a,
